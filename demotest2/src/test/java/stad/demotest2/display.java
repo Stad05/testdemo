8cc0718af16dd01e20fba0e3a7744f9c498e8822
Stad05 <bhagyashree.stad@gmail.com>
2025-09-19 10:35:32 +0530

    Create display.java
    display class added

---
+++ demotest2/src/test/java/stad/demotest2/display.java
@@ -0,0 +1,12 @@
+package stad.demotest2;
+
+public class display {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("in display");
+
+	}
+
+}
